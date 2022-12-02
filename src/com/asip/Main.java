e19876d43c3bbf69d9ec44406b384b2a874ccdfb
Andrei Sidnic <asidnic@guidewire.com>
2022-12-02 23:11:50 +0100

    changes to Main Class

--- src/com/asip/Main.java
+++ src/com/asip/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
        Deck deck = new Deck();
        deck.shuffle();
-       System.out.println(deck.cards);
+        for(Card card : deck.cards){
+            System.out.println(card.getCardName());
+        }
+
     }
 }
